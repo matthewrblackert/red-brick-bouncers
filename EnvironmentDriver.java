fdac17a5ab8f98b5f0dab3b900998f75e0355588
malben05 <52324331+malben05@users.noreply.github.com>
2019-11-04 12:59:12 -0500

    Add files via upload

--- EnvironmentDriver.java
+++ EnvironmentDriver.java
@@ -1,21 +1,31 @@
 package environment;
 
+import java.awt.Color;
+import java.util.*;
 import javax.swing.*;
 
 public class EnvironmentDriver
 {
 	public static void main(String[] args)
 	{
-		JFrame frame = new JFrame("Test");
-		frame.setSize(1500,1000);
+//		ArrayList<Block> ar1 = new ArrayList<Block>();
+//		ar1.add(new Block(1000,75,Color.gray));
+//		ar1.add(new Block(75,650,Color.gray));
+//		ar1.add(new Block(75,650,925,0,Color.gray));
+//		ar1.add(new Block(1000,75,0,575,Color.gray));
+//		ar1.add(new Block(100,100,300,300,Color.orange));
+//		ar1.add(new Block(100,100,700,300,Color.orange));
+		
+		JFrame frame = new JFrame("Stage 1");
+		frame.setSize(1000,650);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JComponent j1 = new Stage();  
-		frame.add(j1);
+		StageBackground b1 = new StageBackground("testBackground.png");
+		Stage s1 = new Stage();
+		s1.back = b1;
+		s1.addBlock(new Block());
+//		Stage s1 = new Stage(ar1); 
+		frame.add(s1);
 		frame.setVisible(true);
+	
 	}
-	
-	/**
-	 * Draws the Block object onto the current JFrame
-	 */
-	
 }
